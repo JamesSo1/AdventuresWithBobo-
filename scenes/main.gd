81ac87b7f6f6e8a780ce5758a59e7adef886614c
James So <jamesso2424@gmail.com>
2025-01-15 21:31:27 -0500

    Set up the game's starting state

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,10 +4,12 @@ extends Node
 const PLAYER_START_POS := Vector2i(150,485)
 const CAM_START_POS := Vector2i(576,324)
 var score : int
+const SCORE_MODIFIER : int = 10
 var speed : float
 const START_SPEED : float = 10.0
 const MAX_SPEED : int = 25
 var screen_size : Vector2i
+var game_running : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,25 +19,38 @@ func _ready() -> void:
 func new_game():
 	#reset the variables
 	score = 0
+	show_score()
+	game_running = false
 	
 	#reset the nodes
 	$Player.position = PLAYER_START_POS
 	$Player.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0,0)
+	
+	#reset HUD
+	$HUD.get_node("StartLabel").show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	speed = START_SPEED
-	
-	#Move player and camera
-	$Player.position.x += speed
-	$Camera2D.position.x += speed
-	
-	#Update score
-	score += speed
-	#print(score)
-	
-	#Update ground position
-	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-		$Ground.position.x += screen_size.x
+	if game_running:
+		speed = START_SPEED
+		
+		#Move player and camera
+		$Player.position.x += speed
+		$Camera2D.position.x += speed
+		
+		#Update score
+		score += speed
+		show_score()
+		
+		#Update ground position
+		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
+		
+func show_score():
+	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
