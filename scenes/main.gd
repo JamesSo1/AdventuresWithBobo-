b9578255b518fbfd48281fa4d3ce1f8d590fa33e
James So <jamesso2424@gmail.com>
2025-01-18 14:13:45 -0500

    Make a functional restart button that pops up when the game ends

--- scenes/main.gd
+++ scenes/main.gd
@@ -31,14 +31,20 @@ var last_obs
 func _ready() -> void:
 	screen_size = get_window().size
 	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
+	$GameOver.get_node("RestartButton").pressed.connect(new_game)
 	new_game()
 	
 func new_game():
-	#reset the variables
+	#Reset the variables
 	score = 0
 	show_score()
 	game_running = false
+	get_tree().paused = false
 	difficulty = 0
+	
+	#Delete any obstacles remanining from the previous game
+	for obs in obstacles:
+		obs.queue.free()
 	
 	#reset the nodes
 	$Player.position = PLAYER_START_POS
@@ -46,9 +52,10 @@ func new_game():
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0,0)
 	
-	#reset HUD
+	#Reset HUD and game over screen
 	$HUD.get_node("StartLabel").show()
-
+	$GameOver.hide()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if game_running:
@@ -72,6 +79,11 @@ func _process(delta: float) -> void:
 		#Update ground position
 		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
 			$Ground.position.x += screen_size.x
+		
+		#Remove obstacles that have gone off screen
+		for obs in obstacles:
+			if obs.position.x < ($Camera2D.position.x - screen_size.x):
+				remove_obs(obs)
 	else:
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
@@ -90,14 +102,33 @@ func generate_obs():
 			var obs_x : int = screen_size.x + score + 100 + (i*100)
 			var obs_y : int = screen_size.y - (ground_height) - (obs_height * obs_scale.y / 2)
 			add_obs(obs, obs_x, obs_y)
+	
+		# Generate a bird obstacle
+		if difficulty == MAX_DIFFICULTY:
+			if (randi() % 2) == 0:
+				obs = bird_scene.instantiate()
+				var obs_x : int = screen_size.x + score + 100
+				var obs_y : int = bird_heights[randi() % bird_heights.size()]
+				add_obs(obs, obs_x, obs_y)
 		
 
 func add_obs(obs, x, y):
 	last_obs = obs
 	obs.position = Vector2i(x, y)
+	obs.body_entered.connect(hit_obs)
 	add_child(obs)
 	obstacles.append(obs)
 	
+func remove_obs(obs):
+	#Removes obstacle node from main node scene
+	obs.queue_free()
+	#Removes obstacle from obstacles array
+	obstacles.erase(obs)
+	
+func hit_obs(body):
+	if body.name == "Player":
+		game_over( )
+
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
 
@@ -106,4 +137,8 @@ func adjust_difficulty():
 	difficulty = (score / SCORE_MODIFIER) / DIFFICULTY_MODIFIER
 	if difficulty > MAX_DIFFICULTY:
 		difficulty = MAX_DIFFICULTY
-		
+
+func game_over():
+	get_tree().paused = true 
+	game_running = false
+	$GameOver.show()
